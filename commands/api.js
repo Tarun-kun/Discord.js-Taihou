fead72fda051583834fd3d93eae4bf2e07462ba9
Tarun-kun <69689706+Tarun-kun@users.noreply.github.com>
2023-02-05 10:22:49 -0500

    Update api.js

--- commands/api.js
+++ commands/api.js
@@ -1,1 +1,13 @@
+const {request} = require('undici');
+const {SlashCommandBuilder} = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName(`neko`)
+    .setDescription('cat will pop-in'),
+    async execute(interaction){
+		const catResult = await request('https://aws.random.cat/meow');
+		const { file } = await catResult.body.json();
+		interaction.reply({ files: [file] });
+    }
+}
